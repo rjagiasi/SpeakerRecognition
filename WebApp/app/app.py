3fab7715bd1f3b8bdeb190d38888666e8a2917ce
sghosalkar <shubhamghosalkar.sg@gmail.com>
2018-03-19 01:18:36 +0530

    Add audio recorder

--- WebApp/app/app.py
+++ WebApp/app/app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
@@ -9,9 +14,28 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/registerSpeaker')
-def registerSpeaker():
-	return render_template('register_speaker.html')
+@app.route('/enrollSpeaker')
+def enrollSpeaker():
+	return render_template('enroll_speaker.html')
+
+
+@app.route('/recognizeSpeaker')
+def recognizeSpeaker():
+	return render_template('recognize_speaker.html')
+
+
+UPLOAD_FOLDER = '/audio/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/upload', methods = ['POST'])
+@cross_origin()
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        app.logger.error('%s logged in successfully', file.filename)
+        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return 'ok'
 
 
 @app.route('/register')
